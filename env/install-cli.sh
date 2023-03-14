1aaabeb9af33afbb7aceaefa6b66993c761ac897
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-03-14 17:18:48 +0200

    Fix go build cmd.

--- env/install-cli.sh
+++ env/install-cli.sh
@@ -5,7 +5,7 @@ set -e
 service=$1
 
 if [[ "$service" == "cli" ]]; then
-  sudo go build -cover ./... -o /bin/findy-agent-vault
+  sudo go build -cover -o /bin/findy-agent-vault
 else
   curl https://raw.githubusercontent.com/findy-network/findy-agent-cli/HEAD/install.sh >install.sh
   chmod a+x install.sh
