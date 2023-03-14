dbcac6a35e9079588e065b4f0703e69123374a20
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-03-14 17:24:49 +0200

    Fix CLI build.

--- env/install-cli.sh
+++ env/install-cli.sh
@@ -5,7 +5,8 @@ set -e
 service=$1
 
 if [[ "$service" == "cli" ]]; then
-  sudo go build -cover -o /bin/findy-agent-vault
+  go build -cover -o findy-agent-cli
+  sudo cp findy-agent-cli /bin
 else
   curl https://raw.githubusercontent.com/findy-network/findy-agent-cli/HEAD/install.sh >install.sh
   chmod a+x install.sh
